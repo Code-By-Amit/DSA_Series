96f6ffb81a1b7d2ec8fb6c8488b09a2cc7ab50df
Amit Saini <167629498+Code-By-Amit@users.noreply.github.com>
2025-04-14 20:15:08 +0530

    78. Subsets

---
+++ getSubsets.cpp
@@ -0,0 +1,21 @@
+// Time Complexity O(2^n * n)  || Space Complexity O(2^n * n)
+class Solution {  
+public:
+    void getSubsets(vector<int>& nums, vector<int>& ans, int i,vector<vector<int>> &allSubsets){
+        if(i==nums.size()){
+            allSubsets.push_back({ans});
+            return;
+        }
+        ans.push_back(nums[i]);
+        getSubsets(nums,ans,i+1,allSubsets);
+        ans.pop_back();
+        getSubsets(nums,ans,i+1,allSubsets);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> allSubsets;
+        vector<int> ans;
+        getSubsets(nums,ans,0,allSubsets);
+        return allSubsets;
+    }
+};
