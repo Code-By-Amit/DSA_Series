ccb2134136787f64efdc6deb6ba542385758122c
Amit Saini <167629498+Code-By-Amit@users.noreply.github.com>
2025-09-08 13:50:00 +0530

    1480. Running Sum of 1d Array

---
+++ runningSum.cpp
@@ -0,0 +1,13 @@
+// TC: O(n) || SC = O(1)
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int currentSum = nums[0];
+
+        for(int i=1;i<nums.size();i++){
+            nums[i] += nums[i-1];
+        }
+        return nums;
+    }
+};
