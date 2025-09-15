e7a3df5fc377be7d77bb805e388291279cb16588
Amit Saini <167629498+Code-By-Amit@users.noreply.github.com>
2025-09-15 18:41:22 +0530

    2011. Final Value of Variable After Performing Operations

---
+++ finalValueAfterOperations.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x = 0;
+        for(auto operation :operations ){
+            if(operation == "X++" || operation == "++X") x++;
+            else x--;
+        }
+        return x;
+    }
+};
