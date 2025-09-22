4267f38484d7d85a7c2b01f119c160b67ac66072
Amit Saini <167629498+Code-By-Amit@users.noreply.github.com>
2025-09-22 10:57:17 +0530

    1991. Find the Middle Index in Array

---
+++ pivotIndex.cpp
@@ -0,0 +1,17 @@
+// Time Complexity: O(n) || Space Compelxity: O(1)
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        int totalSum = 0;
+        for(int num:nums) totalSum+=num;
+
+        int leftSum = 0;
+        for(int i=0; i<n;i++){
+            if(leftSum == totalSum - leftSum - nums[i]) return i;
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+};
