e0b7fd19ef1bd1604a00661de64dd7b96cea6d0c
Amit Saini <167629498+Code-By-Amit@users.noreply.github.com>
2025-10-04 14:54:26 +0530

    832. Flipping an Image

---
+++ flipAndInvertImage.cpp
@@ -0,0 +1,25 @@
+// TC: O(m*n) || SC: O(1) 
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+        int n = image.size();
+        int m = image[0].size();
+
+        for (int i = 0; i < n; i++) {
+            int lp = 0;
+            int rp = image[i].size() - 1;
+            while (lp <= rp) {
+                if (lp == rp) {
+                    image[i][rp] ^= 1;
+                } else {
+                    image[i][lp] ^= 1;
+                    image[i][rp] ^= 1;
+                    swap(image[i][lp], image[i][rp]);
+                }
+                lp++; rp--;
+            }
+        }
+
+        return image;
+    }
+};
