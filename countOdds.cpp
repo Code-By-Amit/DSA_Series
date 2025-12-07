b73267d878e181c6eb03e90d1c88b82bf6c44d86
Amit Saini <167629498+Code-By-Amit@users.noreply.github.com>
2025-12-07 12:11:17 +0530

    1523. Count Odd Numbers in an Interval Range

---
+++ countOdds.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        return (high+1)/2 - low/2;
+    }
+};
+
+
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        int ans = 0;
+        for(int i=low;i<=high;i++){
+            if(i%2!=0) ans++;
+        }
+        return ans;
+    }
+};
